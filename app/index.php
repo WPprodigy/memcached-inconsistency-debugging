972ba1c9afe6ba4785f30e83f806a1f34783774c
Caleb Burks <caleb.burks@automattic.com>
2023-07-17 14:28:02 -0500

    Replication!

--- app/index.php
+++ app/index.php
@@ -3,101 +3,65 @@
 class Memcached_Testing {
 	public Memcached $mc;
 
-	const CANARY_KEY = 'canary';
-
 	function __construct() {
-		$mc = new Memcached( 'persistent-123' );
+		$mc = new Memcached( 'persistent' );
 
 		// Only add servers and set options if not already available (to keep persistency).
 		if ( empty( $mc->getServerList() ) ) {
 			$mc->addServers( [ [ 'memcached-1', 11211, 1 ], [ 'memcached-2', 11211, 1 ] ] );
 
+			// NOTE: Make sure to change the persistent ID if changing these options while testing.
 			$mc->setOptions( [
-				Memcached::OPT_BINARY_PROTOCOL => false,
-				Memcached::OPT_SERIALIZER      => Memcached::SERIALIZER_PHP, // TODO: igbinary
-				Memcached::OPT_CONNECT_TIMEOUT => 1000,
-				Memcached::OPT_COMPRESSION     => true,
-				Memcached::OPT_TCP_NODELAY     => true,
+				// What we have in production
+				// Memcached::OPT_CONNECT_TIMEOUT => 1000,
+				// Memcached::OPT_TCP_NODELAY     => true,
+
+				// This one appears required to replicate the bug:
+				Memcached::OPT_POLL_TIMEOUT => 20, // milliseconds (default 1000)
+
+				// These all seem to increase the likelihood, but are not technically required:
+				// Memcached::OPT_RETRY_TIMEOUT   => 60, // seconds (default 0)
+				// Memcached::OPT_CONNECT_TIMEOUT => 20, // milliseconds (default 1000)
+				// Memcached::OPT_RECV_TIMEOUT    => 20*1000, // microseconds (default 0)
+				// Memcached::OPT_SEND_TIMEOUT    => 20*1000, // microseconds (default 0)
+				// Memcached::OPT_TCP_NODELAY     => true, // (default false)
 			] );
 		}
 
 		$this->mc = $mc;
 	}
 
-	function get_and_validate_canary() {
-		$value = $this->mc->get( self::CANARY_KEY );
+	function get_and_validate_key( $key ) {
+		$value = $this->mc->get( $key );
 
-		if ( empty( $value ) ) {
-			if ( Memcached::RES_NOTFOUND !== $this->mc->getResultCode() ) {
-				if ( false !== $value ) {
-					error_log( 'GET canary result code: ' . $this->mc->getResultCode() . '. Value: ' . print_r( $value, true ) . '. Type: ' . gettype( $value ) );
-				}
-			}
-
-			return [];
-		}
-
-		// This memcached value should only ever be an an array of keys that have values of only "true".
-		if ( [ true ] !== array_unique( array_values( $value ) ) ) {
-			trigger_error( 'ERROR!!!!!! An invalid value was found in the cache key: ' . print_r( $value, true ), E_USER_WARNING );
-			die( 'Ruh roh' );
+		if ( $value !== false && $key !== $value ) {
+			trigger_error( 'Uh oh! An invalid value was returned from the cache key (' . $key . '): ' . print_r( $value, true ), E_USER_WARNING );
 		}
 
 		return $value;
 	}
 
 	function set( $key, $value ) {
-		$result = $this->mc->set( $key, $value, 0 );
-
-		if ( ! $result && $key === self::CANARY_KEY && $this->mc->getResultCode() !== 47 ) {
-			trigger_error( 'Unable to set key: ' . $key . ' ' . $this->mc->getResultCode(), E_USER_WARNING );
-		}
-	}
-
-	function get( $key ) {
-		return $this->mc->get( $key );
-	}
-
-	function getMulti( $keys ) {
-		return $this->mc->getMulti( $keys );
-	}
-
-	function delete( $key ) {
-		return $this->mc->delete( $key );
+		$this->mc->set( $key, $value, 0 );
 	}
 }
 
 $memcached = new Memcached_Testing();
 
-$multiGetKeys = array_map( fn( $n ) => 'random-' . $n, range( 1, 1000 ) );
+if ( isset( $_GET['set_keys'] ) ) {
+	trigger_error( 'Setting up key caches', E_USER_NOTICE );
+}
 
 // Run the stuffs
-$max = 100;
+$max = isset( $_GET['set_keys'] ) ? 50 : mt_rand( 1, 50 );
 for ( $i = 0; $i <= $max; $i++ ) {
-	// The first thing a request sends for.
-	$canary_value = $memcached->get_and_validate_canary();
+	$loop_key = 'loop_' . $i;
 
-	// A chunky getMulti to stress out MC a little maybe?
-	$memcached->getMulti( $multiGetKeys );
-
-	// Add some other random noise.
-	$memcached->set( 'random-' . $i, [ 'something' => 'else #' . mt_rand( 0, 1000 ) ] );
-	$memcached->get( 'random-' . $i );
-
-	// Sometimes update the canary.
-	if ( mt_rand( 0, 100 ) === 1 ) {
-		$canary_value = $memcached->get_and_validate_canary();
-		$canary_value[ 'string-' . mt_rand( 0, 1000 ) ] = true;
-		$memcached->set( $memcached::CANARY_KEY, $canary_value );
+	if ( isset( $_GET['set_keys'] ) ) {
+		$memcached->set( $loop_key, $loop_key );
 	}
 
-	// Less frequently delete the canary.
-	if ( count( $canary_value ) > 1000 ) {
-		$memcached->delete( $memcached::CANARY_KEY );
-	}
-
-	// The last thing a request sends for
-	$memcached->get( 'random-' . $i );
+	$memcached->get_and_validate_key( $loop_key );
 }
 
 echo "All Done";
--- readme.md
+++ readme.md
@@ -1,16 +1,10 @@
 Like https://github.com/WPprodigy/memcache-inconsistency-debugging, but with a new flavor 🙃
 
-# Attempting to replicate Memcached consistency issue
+# Replicate Memcached consistency issue
 
 1) Run `docker compose up` to build up the environment.
 2) Run `./page-requester` to start up some threads requesting the test site/script.
 3) Run `./memcached-signal-sender` at the same time to immitate resource contention on a mc server.
 4) View the PHP logs error logs and see if any anomalies occur.
 
-Unable to replicate so far, so we're still missing some unknown factors. Have tried tweaking various iteration numbers and sleep durations all around, no luck so far. Have also tried fully stopping/starting the memcached container itself.
-
-The following error codes are returned from `Memcached::getResultCode()`:
-
-- When the server has been turned off: 47 = MEMCACHED_SERVER_TEMPORARILY_DISABLED
-- When the server has been SIGSTOP'd: 31 = MEMCACHED_TIMEOUT
-- Sometimes after the server has been turned off (presumably when it's a fresh php fpm child): 03 = MEMCACHED_CONNECTION_FAILURE
+Issue: https://github.com/php-memcached-dev/php-memcached/issues/531
